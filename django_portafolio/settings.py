3225cea5cad0e641a52fb2cdf12558b49588f522
jesusjg3 <maciaslugojesusfernando@gmail.com>
2023-09-19 20:55:32 -0500

    'primer intento'

--- django_portafolio/settings.py
+++ django_portafolio/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-1229xl#5c%ywz1dj9u2&ku_-6@75wpvd%uj5th$0$09njhzw$o'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
